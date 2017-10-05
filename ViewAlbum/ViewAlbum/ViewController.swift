48655d92b90d82053c45c936b9a843b4f6bf7e4e
majinglian2015 <majinglian2015.xust@aliyun.com>
2017-10-05 15:58:39 +0800

    init project

---
+++ ViewAlbum/ViewAlbum/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ViewAlbum
+//
+//  Created by mjl on 17/10/5.
+//  Copyright © 2017年 bigtree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let number = 10
+        var success = 0
+        success = 1
+        print(number);
+        print(success);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
